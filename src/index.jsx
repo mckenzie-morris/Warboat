279afdb922c79f5a2963715c5891c494d5d0998c
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-12-05 11:16:04 -0800

    pages added 12/5/24

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,6 @@ import './styles.css';
 import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
 
-const root = createRoot(document.getElementById('app'));
+const root = createRoot(document.getElementById('root'));
 
 root.render(<App />);
