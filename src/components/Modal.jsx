5e79a892866a6f6a4a67aadfea0711307edad996
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-12-12 16:03:04 -0800

    carousel component integrated 12/12/24

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,6 @@
 import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
+import Carousel from "./Carousel.jsx";
 
 function BasicModal() {
   const [open, setOpen] = React.useState(false);
@@ -14,14 +15,18 @@ function BasicModal() {
       >
         Open modal
       </Button>
-      <Modal open={open} onClose={handleClose}>
-        <div className="fixed left-1/2 top-1/2 h-[250px] w-[250px] -translate-x-1/2 -translate-y-1/2 bg-red-500">
+      {/* change open={true} to open={open} after finishing edits */}
+      <Modal open={true} onClose={handleClose}>
+        <div className="fixed left-1/2 top-1/2 h-[750px] w-[750px] -translate-x-1/2 -translate-y-1/2 bg-red-500">
           <Button
             className="rounded-md bg-green-600 px-4 py-1"
             onClick={handleClose}
           >
             Close!{" "}
           </Button>
+          <div className="relative top-1/2 -translate-y-1/2">
+            <Carousel />
+          </div>
         </div>
       </Modal>
     </div>
