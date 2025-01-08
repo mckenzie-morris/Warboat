08dc726c0e207187a7ad635821f95c2a281db1d4
mckenzie-morris <mckenzie.morris1111@gmail.com>
2025-01-07 16:26:19 -0800

    react slick carousel integrated, in progress 1/7/25

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,3 +1,7 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import pastel from "../assets/themes/pastel.png";
 import retro from "../assets/themes/retro.png";
 import neon from "../assets/themes/neon.png";
@@ -11,7 +15,67 @@ import temperate from "../assets/themes/temperate.png";
 import tropical from "../assets/themes/tropical.png";
 import sunset from "../assets/themes/sunset.png";
 
+const themes = [
+  pastel,
+  retro,
+  neon,
+  gold,
+  spring,
+  summer,
+  fall,
+  winter,
+  space,
+  temperate,
+  tropical,
+  sunset,
+];
 
-export default () => {
-  return
-};
+function NextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red", right: '3%', zIndex: '1' }}
+      onClick={onClick}
+    />
+  );
+}
+
+function Carousel() {
+  const settings = {
+    arrows: true,
+    nextArrow: <NextArrow />,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+    ],
+  };
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        {themes.map((theme, idx) => {
+          return (
+            <div key={idx} className="">
+              <img className="mx-auto p-5" src={theme}></img>
+            </div>
+          );
+        })}
+      </Slider>
+    </div>
+  );
+}
+
+export default Carousel;
