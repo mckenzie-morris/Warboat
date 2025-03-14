6e5d9610cc5185c656a8c87cce37e4d1ebf36d3b
mckenzie-morris <mckenzie.morris1111@gmail.com>
2025-03-14 16:34:51 -0700

    Profile page refresh issue fixed, Loading page added 3/14/25

---
+++ client/src/pages/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+      <div>
+        <h1 className="text-9xl">Loading...</h1>
+      </div>
+    );
+  };
+  
+  export default Loading;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,65 +1,80 @@
 import Button from "@mui/material/Button";
-
-
-import { Link, Navigate } from "react-router";
-// import { useNavigate } from "react-router";
-
-
+import { Link } from "react-router";
+import { useNavigate } from "react-router";
+import Loading from "./Loading.jsx";
 import Hero from "../components/Hero.jsx";
 import { ProfileContext } from "../index.jsx";
 import { instanceWithInterceptor } from "../apis/interceptor.js";
+import React from "react";
 
 const Profile = () => {
   const { isLoggedIn, setLoggedIn } = React.useContext(ProfileContext);
-  const [serverRes, setServerRes] = React.useState(null)
-  // const navigate = useNavigate();
-  // if (!isLoggedIn) {
-  //   return navigate("/login");
-  // }
+  const [serverResponse, setServerResponse] = React.useState(null);
+  const navigate = useNavigate();
+  // initialize a ref to null
+  const loggedInRef = React.useRef(null);
 
-  // if (!isLoggedIn) {
-  //   return <Navigate to={"/login"} />;
-  // }
-  
   React.useEffect(() => {
+    // map 'loggedInRef' to 'isLoggedIn' state
+    loggedInRef.current = isLoggedIn;
+    console.log("loggedInRef.current: ", loggedInRef.current);
+  }, [isLoggedIn]);
 
-    const grabProtectedStuff = async () => {
+  React.useEffect(() => {
+    const delayedRequest = async () => {
+      /* if after 500ms there is still not a a user to reference in 'loggedInRef', 
+      redirect the user to '/login' page */
+      if (!loggedInRef.current) {
+        return navigate("/login");
+      }
       try {
-        const response = await instanceWithInterceptor.get(
-          // when running just the server, change to: "/profile"
-          "/profile",
-          {
-            headers: { Authorization: `Bearer ${isLoggedIn[0].accessToken}` },
+        const response = await instanceWithInterceptor.get("/profile", {
+          headers: {
+            /* if 'isLoggedIn' state's access token is still valid when this page is accessed, 
+              or the necessary amount of time has elapsed for the 'ProfileProvider' to return a new 
+              access token from the '/refresh' endpoint, send the access token on the appropriate
+               request header */
+            Authorization: `Bearer ${loggedInRef.current[0].accessToken}`,
+          },
 
-            // attach setState function 'setLoggedIn' to request config
-            setLoggedIn: setLoggedIn,
+          // attach setState function 'setLoggedIn' to request config
+          setLoggedIn: setLoggedIn,
 
-            /* `withCredentials` indicates whether or not cross-site Access-Control requests
+          /* `withCredentials` indicates whether or not cross-site Access-Control requests
             should be made using credentials (such as cookies, authentication headers or 
             TLS client certificates) */
-            withCredentials: true,
-          },
-        );
+          withCredentials: true,
+        });
         console.log("🚩 successful axios request: ", response.data);
-        setServerRes(response.data)
+        setServerResponse(response.data);
       } catch (error) {
         console.log("💩💩💩", error.response?.data);
       }
     };
-    grabProtectedStuff();
+
+    /* provide ample time for for the ProfileProvider to mount (on initial 'root' render) and 
+    return a new access token from the '/refresh' endpoint if refresh token is present (and valid)
+    on HTTP-only secure cookie */
+    setTimeout(delayedRequest, 500);
   }, []);
 
   return (
     <div>
-      <Hero displayText="profile" />
-      {!serverRes ? 'waiting...' : Object.entries(serverRes).map(([key, val], idx) => {
-        return <p key={idx}>{`${key}: ${val}`}</p>
-      })}
-      <Link to="/">
-        <Button className="rounded-md bg-green-600 px-4 py-1">
-          Return Home
-        </Button>
-      </Link>
+      {!serverResponse ? (
+        <Loading />
+      ) : (
+        <div>
+          <Hero displayText="profile" />
+          {Object.entries(serverResponse).map(([key, val], idx) => {
+            return <p key={idx}>{`${key}: ${val}`}</p>;
+          })}
+          <Link to="/">
+            <Button className="rounded-md bg-green-600 px-4 py-1">
+              Return Home
+            </Button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
