af3d66ead6991d8b0181f4ffdb1eb48e6072fb4f
mckenzie-morris <mckenzie.morris1111@gmail.com>
2025-03-21 15:59:46 -0700

    modularized Modal 3/21/25

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,8 +1,7 @@
 import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
-import Carousel from "./Carousel.jsx";
 
-const ModalWithCarousel = () => {
+const ModularModal = ({ displayTextOpenButton, displayTextCloseButton, modalContent }) => {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -13,7 +12,7 @@ const ModalWithCarousel = () => {
         className="rounded-md bg-green-600 px-4 py-1"
         onClick={handleOpen}
       >
-        Open modal
+        {displayTextOpenButton}
       </Button>
       {/* change open={true} to open={open} after finishing styling edits */}
       <Modal open={open} onClose={handleClose}>
@@ -21,13 +20,13 @@ const ModalWithCarousel = () => {
           id="modal-content"
           className="fixed left-1/2 top-1/2 flex max-h-full max-w-full -translate-x-1/2 -translate-y-1/2 flex-col justify-center bg-gray-500"
         >
-          <Carousel />
+          {modalContent}
 
           <Button
             className="mx-auto mb-5 mt-10 rounded-md bg-green-600 px-4 py-1"
             onClick={handleClose}
           >
-            Close!
+            {displayTextCloseButton}
           </Button>
         </div>
       </Modal>
@@ -35,4 +34,4 @@ const ModalWithCarousel = () => {
   );
 };
 
-export default ModalWithCarousel;
+export default ModularModal;
--- client/src/pages/Options.jsx
+++ client/src/pages/Options.jsx
@@ -1,13 +1,18 @@
-import ModalWithCarousel from "../components/ModalWithCarousel.jsx";
+import Modal from "../components/Modal.jsx";
 import Button from "@mui/material/Button";
 import { Link } from "react-router";
 import Hero from "../components/Hero.jsx";
+import Carousel from "../components/Carousel.jsx";
 
 const Options = () => {
   return (
     <div>
       <Hero displayText="options" />
-      <ModalWithCarousel />
+      <Modal
+        displayTextOpenButton="Select Theme"
+        displayTextCloseButton="Close!"
+        modalContent={<Carousel/>}
+      />
       <Link to="/">
         <Button className="rounded-md bg-green-600 px-4 py-1">
           Return Home
